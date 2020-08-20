e9f6d103763cccdc37110ee547dca205c2f2ff78
Nicolas Blanco <me@nicolasblan.co>
2020-08-21 01:08:06 +0300

    Deps

--- lib/flex_auth/plug/stored_session_plug.ex
+++ lib/flex_auth/plug/stored_session_plug.ex
@@ -3,16 +3,31 @@ defmodule FlexAuth.Plug.StoredSessionPlug do
     quote do
       import Plug.Conn
 
-      defp get_resource_function(), do: unquote(Keyword.fetch!(opts, :get_resource_function))
+      defp get_resource(), do: unquote(Keyword.fetch!(opts, :get_resource))
       defp session_module(), do: unquote(Keyword.fetch!(opts, :session_module))
       defp endpoint_module(), do: unquote(Keyword.fetch!(opts, :endpoint_module))
       defp resource_name(), do: unquote(Keyword.fetch!(opts, :resource_name))
       defp token_timeout(), do: unquote(Keyword.get(opts, :token_timeout, 806_400))
 
+      defp call_resource_function(mfa, conn, private_resource_id) when is_tuple(mfa) do
+        apply(
+          elem(mfa, 0),
+          elem(mfa, 1),
+          [conn, private_resource_id] ++ elem(2)
+        )
+      end
+
+      defp call_resource_function(function, conn, private_resource_id)
+           when is_function(function) do
+        apply(function, [private_resource_id])
+      end
+
       defp fetch_auth_resource(conn) do
+        private_resource_id = conn.private[:"auth_#{resource_name()}_id"]
+
         resource =
-          conn.private[:"auth_#{resource_name()}_id"] &&
-            apply(get_resource_function(), [conn.private[:"auth_#{resource_name()}_id"]])
+          private_resource_id &&
+            call_resource_function(get_resource(), conn, private_resource_id)
 
         resource_id = if resource, do: resource.id, else: nil
 
